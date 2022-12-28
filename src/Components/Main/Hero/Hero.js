54bfb6ec035ea97158239420bcdca2e6a16e552d
TomasCalabria <totocalabria.tc@gmail.com>
2022-12-28 14:03:39 -0300

    modificando banner

--- src/Components/Main/Hero/Hero.js
+++ src/Components/Main/Hero/Hero.js
@@ -9,16 +9,17 @@ import banner from '../../../Media/banner_3.jpg'
 export const Hero = () => {
 
   return (<>
-  <section className='hero' style={{width:'100%',height:'500px',display:'flex',flexDirection:'row'}} >
+  <section className='hero' style={{width:'100%',height:'550px',display:'flex',flexDirection:'row'}} >
     <div style={{width:'100%',height:'100%',justifyContent:'center',textAlign:'center',marginTop:'0.5rem',alignContent:'center'  }}>
     <div className='banner'>
     <img src={banner}></img>
-    <div className='banner_text'>
-    <h2>Colección 2023</h2>
-    <button className='banner_arrow'><img src={gold_arrow}></img></button>
-    </div>
-    </div>
-    
+      <div className='banner_text_container'>
+        <div className='banner_text'>
+          <h2>Colección 2023</h2>
+           <button className='banner_arrow'><img src={gold_arrow}></img></button>
+          </div>
+        </div>
+      </div>
     </div>
     </section>
     </>
