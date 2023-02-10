1faa19c6f0a06343d6a80f5857e865a065d788dc
tomascalabria <totocalabria.tc@gmail.com>
2023-02-10 11:25:39 -0300

    añadiendo alt a las imagenes para mejorar SEO

--- src/Components/Main/Hero/Hero.js
+++ src/Components/Main/Hero/Hero.js
@@ -12,11 +12,11 @@ export const Hero = () => {
   <section className='hero' style={{width:'100%',height:'550px',display:'flex',flexDirection:'row'}} >
     <div style={{width:'100%',height:'100%',justifyContent:'center',textAlign:'center',marginTop:'0.5rem',alignContent:'center'  }}>
     <div className='banner'>
-    <img src={banner}></img>
+    <img alt='Banner de La Tiendita. Coleccion 2023' src={banner}></img>
       <div className='banner_text_container'>
         <div className='banner_text'>
           <h2>Colección 2023</h2>
-           <button className='banner_arrow'><img src={gold_arrow}></img></button>
+           <button className='banner_arrow'><img alt='Flecha amarilla para ver los productos de la coleccion 2023' src={gold_arrow}></img></button>
           </div>
         </div>
       </div>
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { NavBar } from '../Navbar/NavBar'
 import { Categories } from './Categories/Categories'
 import { Hero } from './Hero/Hero'
 import shoes_categories_portrait from '../../Media/Shoe_categories_portrait.webp'
